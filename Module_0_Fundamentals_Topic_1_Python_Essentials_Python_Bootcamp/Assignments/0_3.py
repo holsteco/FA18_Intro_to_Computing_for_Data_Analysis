689b0c7f3ab31723ed3cfd33d03efdc4ba23d118
holsteco <39134537+holsteco@users.noreply.github.com>
2018-08-26 09:45:41 -0400

    updates

---
+++ Module_0_Fundamentals_Topic_1_Python_Essentials_Python_Bootcamp/Assignments/0_3.py
@@ -0,0 +1,12 @@
+def strcat_list(L):
+    assert type(L) is list
+	#reverse the list
+    L.reverse()
+	#concatenate the list
+	result = 
+    return result
+
+#the list	
+L = ['abc', 'def', 'ghi']
+
+result = strcat_list(L)
